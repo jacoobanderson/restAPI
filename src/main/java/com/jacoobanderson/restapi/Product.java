8b5ff5b6f6eb8756c0564f7fd679d45907d7324f
jacoobanderson <j00cwa@gmail.com>
2022-09-12 17:57:34 +0200

    Create insert product functionality

--- src/main/java/com/jacoobanderson/restapi/Product.java
+++ src/main/java/com/jacoobanderson/restapi/Product.java
@@ -1,4 +1,6 @@
 package com.jacoobanderson.restapi;
+import java.time.Instant;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
@@ -7,8 +9,16 @@ import lombok.Data;
 @Document
 public class Product {
     @Id
-    private String id;
+    private long id;
     private String name;
     private String description;
     private int price;
+    private Instant create;
+    
+    public Product(String name, String description, int price, Instant create) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.create = create;
+    }
 }
